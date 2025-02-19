0834db7a1900fc90d1fb438735577a4c9fd3fede
apastrana <apastrana6@gatech.edu>
2025-02-19 12:18:18 -0500

    Fix mobile icon bug and add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Public_Sans } from "next/font/google";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Public_Sans({ subsets: ["latin"], weight: "300"});
@@ -24,6 +25,7 @@ export default function RootLayout({
         <div className="flex min-h-screen flex-col">
           {children}
           <SpeedInsights />
+          <Analytics />
         </div>
       </body>
     </html>
